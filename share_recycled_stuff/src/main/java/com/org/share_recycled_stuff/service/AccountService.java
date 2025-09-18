6d07287711f32b4be1ffd198c479608f75094cd4
Egodz93 <than624264@gmail.com>
2025-09-18 21:29:26 +0700

    SCS-10 Register Account

---
+++ share_recycled_stuff/src/main/java/com/org/share_recycled_stuff/service/AccountService.java
@@ -0,0 +1,31 @@
+package com.org.share_recycled_stuff.service;
+
+import com.org.share_recycled_stuff.entity.Account;
+import com.org.share_recycled_stuff.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void register(Account account) {
+        // Check email trùng
+        if (accountRepository.existsByEmail(account.getEmail())) {
+            throw new RuntimeException("Email đã tồn tại!");
+        }
+
+        // Gán giá trị mặc định
+        account.setVerificationToken(UUID.randomUUID().toString());
+        account.setCreatedAt(LocalDateTime.now());
+        accountRepository.save(account);
+
+    }
+}
