3ad5e0a1646d62b532970555399bde381689c83c
Egodz93 <than624264@gmail.com>
2025-09-19 22:30:57 +0700

    SCS-10 update Register

--- share_recycled_stuff/src/main/java/com/org/share_recycled_stuff/service/RegisterService.java
+++ share_recycled_stuff/src/main/java/com/org/share_recycled_stuff/service/RegisterService.java
@@ -1,32 +1,10 @@
 package com.org.share_recycled_stuff.service;
-
-import com.org.share_recycled_stuff.entity.Account;
-import com.org.share_recycled_stuff.repository.AccountRepository;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
+import com.org.share_recycled_stuff.dto.request.RegisterRequest;
+import com.org.share_recycled_stuff.dto.response.ApiResponse;
 
 
-@Service
-public class AccountService {
+import java.util.Optional;
 
-    private final AccountRepository accountRepository;
-
-    public AccountService(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
-
-    public void register(Account account) {
-        // Check email trùng
-        if (accountRepository.existsByEmail(account.getEmail())) {
-            throw new RuntimeException("Email đã tồn tại!");
-        }
-
-        // Gán giá trị mặc định
-        account.setVerificationToken(UUID.randomUUID().toString());
-        account.setCreatedAt(LocalDateTime.now());
-        accountRepository.save(account);
-
-    }
+public interface RegisterService {
+    ApiResponse<Void> register(RegisterRequest request);
 }
--- share_recycled_stuff/src/main/java/com/org/share_recycled_stuff/service/impl/RegisterServiceImpl.java
+++ share_recycled_stuff/src/main/java/com/org/share_recycled_stuff/service/impl/RegisterServiceImpl.java
@@ -1,4 +1,27 @@
 package com.org.share_recycled_stuff.service.impl;
 
-public class RegisterServiceImpl {
+
+import com.org.share_recycled_stuff.dto.request.RegisterRequest;
+import com.org.share_recycled_stuff.dto.response.ApiResponse;
+import com.org.share_recycled_stuff.entity.Account;
+import com.org.share_recycled_stuff.repository.AccountRepository;
+import com.org.share_recycled_stuff.service.RegisterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterServiceImpl implements RegisterService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public ApiResponse<Void> register(RegisterRequest request) {
+        Account account = new Account();
+
+        account.setEmail(request.getEmail());
+        account.setPassword(request.getPassword());
+        accountRepository.save(account);
+        return ApiResponse.noti(200, "Đăng ký thành công", "/api/auth/register");
+    }
 }
