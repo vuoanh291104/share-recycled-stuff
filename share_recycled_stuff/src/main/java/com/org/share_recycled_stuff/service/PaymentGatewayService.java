547c137b6b665ed15569d8ecb2dd102dcadc79af
Egodz93 <than624264@gmail.com>
2025-11-15 10:45:34 +0700

    SCS-80 Proxyseller commission payment

--- share_recycled_stuff/src/main/java/com/org/share_recycled_stuff/service/PaymentGatewayService.java
+++
@@ -1,7 +0,0 @@
-package com.org.share_recycled_stuff.service;
-
-import java.math.BigDecimal;
-
-public interface PaymentGatewayService {
-    String createPaymentUrl(BigDecimal totalAmount, String transactionId, String orderInfo, String ipAddress);
-}
